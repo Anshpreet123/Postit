390b9f16a7a38d6c6321844c478fc665c5b839c7
Anshpreet22 <210100018@iitb.ac.in>
2025-04-06 15:15:41 +0530

    changed the forntend url for connection

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
-let BASE_URL = "https://post-it-heroku.herokuapp.com/";
+let BASE_URL = "http://13.51.72.239:4000/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:4000/";
+  BASE_URL = "http://13.51.72.239:4000/";
 }
 
 export { BASE_URL };
